bc7500d8289a6dc6f6b4b12bde29c1640aa97721
jl10102727 <jl10102727@gmail.com>
2018-07-02 15:15:05 +0800

    15-15-04

--- auto.sh
+++ auto.sh
@@ -12,11 +12,11 @@ for f in $FILES
 do
 /mingw64/bin/git add $f
 /mingw64/bin/git commit -m "$(date +%H-%M-%S)"
-/mingw64/bin/git remote add https://github.com/Jacky0123456/test.git
+/mingw64/bin/git remote add origin https://github.com/Jacky0123456/test.git
 /mingw64/bin/git push -u origin master
 /mingw64/bin/git remote remove origin
 done
 fi;
-rm -rf /c/Users/Orozco/test/pic/*
+rm -rf /e/git/test/pic/*
 sleep 3
 done
